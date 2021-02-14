c29ee5f2d4f7201a6b6003051b254d8df1cf8318
Nevin Herring <nevin.herring@gmail.com>
2021-02-14 15:24:41 -0600

    more operator fun

--- Program.cs
+++ Program.cs
@@ -10,6 +10,9 @@ namespace Operators
             int a = 3;
             int b = a++;
             WriteLine($"a is {a}, b is {b}");
+            int c = 3;
+            int d = ++c;
+            WriteLine($"c is {c}, d is {d}");
         }
     }
 }
